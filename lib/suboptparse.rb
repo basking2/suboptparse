fbe5327586ea805e8da4bb9a1792f9d3e200ab6e
Sam Baskinger <basking2@yahoo.com>
2025-01-24 00:42:04 -0600

    Hacking

--- lib/suboptparse.rb
+++ lib/suboptparse.rb
@@ -2,7 +2,60 @@
 
 require_relative "suboptparse/version"
 
+require 'optparse'
+
 module Suboptparse
   class Error < StandardError; end
   # Your code goes here...
+end
+
+class SubOptParser
+  def initialize(*args)
+    @op = OptionParser.new(*args)
+    @op.raise_unknown = false
+    @banner = @op.banner
+    @cmd = proc {
+      puts @op.help
+    }
+    @cmds = {}
+
+    yield(self) if block_given?
+  end
+
+  def method_missing(name, *args, &block)
+    @op.__send__(name, *args, &block)
+  end
+
+  # Add a command (and return the resulting command).
+  def addcmd(name, description, &body)
+    @cmds[name] = {
+      description: description,
+      body: body,
+    }
+
+    @op.banner = @banner + cmdhelp
+  end
+
+  def setcmd(&body)
+    @cmd = body
+  end
+
+  def cmdhelp
+    @cmds.inject("\n\n") do |h, v|
+      h += "#{v[0]} - #{v[1][:description]}\n"
+    end + "\n"
+  end
+
+  def parse(*args, into: nil)
+    self.parse!(args)
+
+    if args.length > 0 && @cmds.length > 0
+      name = args.shift
+      cmd = cmds[name]
+      raise Error.new("Subcommand #{name} was not found.") if cmd.nil?
+
+      cmd.parse!(args)
+    end
+  end
+
 end
--- spec/suboptparse_spec.rb
+++ spec/suboptparse_spec.rb
@@ -6,6 +6,35 @@ RSpec.describe Suboptparse do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
+  end
+
+  it "normal opt works" do
+    i = 0
+    SubOptParser.new() do |opt|
+      opt.on('-i=int', Integer, 'Add i') do |v|
+        i = v
+      end
+    end.parse(*['-i', '4'])
+
+    expect(i).to be_an(Integer)
+    expect(i).to eq(4)
+  end
+
+  it "prints a banner" do
+    o = SubOptParser.new do |opt|
+      opt.addcmd("cmd1", "this does things") { puts "cmd1" }
+      opt.on('-i=int', "Add an int.") {}
+    end
+
+    o.addcmd("cmd2", "this does other things") { puts "cmd2" }
+
+    expect('''Usage: rspec [options]
+
+cmd1 - this does things
+cmd2 - this does other things
+
+    -i=int                           Add an int.
+''').to eq(o.help)
   end
 end
